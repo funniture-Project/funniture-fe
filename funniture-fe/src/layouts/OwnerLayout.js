24478ff3e3d354c34f9776c90c5e97a3f75c11d5
jin <jin200126@gmail.com>
2025-02-10 02:01:20 +0900

    :lipstick: 전체 페이지 레이아웃 잡기 :lipstick:

--- funniture-fe/src/layouts/OwnerLayout.js
+++ funniture-fe/src/layouts/OwnerLayout.js
@@ -1,5 +1,55 @@
+import './ownerlayout.css'
+import { Outlet } from "react-router-dom"
+import { NavLink } from 'react-router-dom';
+import profileImg from '../assets/images/profiletest.jpg'
+
 function OwnerLayout() {
 
+    const actstyle = {
+        backgroundColor: "#7F5539",
+        color: "#ffffff"
+    }
+
+    return (
+        <div className="owner">
+            <div className="ownerMenuBar">
+                <div className='ownerInfo'>
+                    <img src={profileImg} alt="프로필 이미지" />
+                    <div className='name'>이은미 님</div>
+                    <div className='email'>testEmail@gmail.com</div>
+                </div>
+
+                <div className="menus">
+                    <NavLink to="/owner" className={({ isActive }) => (isActive ? "selectedMenu" : "")}>
+                        <div>메뉴</div>
+                    </NavLink>
+
+                    <NavLink to="/" style={({ isActive }) => isActive ? actstyle : undefined}>
+                        <div>메뉴</div>
+                    </NavLink>
+
+                    <NavLink to="/" style={({ isActive }) => isActive ? actstyle : undefined}>
+                        <div>메뉴</div>
+                    </NavLink>
+
+                    <NavLink to="/" style={({ isActive }) => isActive ? actstyle : undefined}>
+                        <div>메뉴</div>
+                    </NavLink>
+
+                    <NavLink to="/" style={({ isActive }) => isActive ? actstyle : undefined}>
+                        <div>메뉴</div>
+                    </NavLink>
+                    <NavLink to="/" style={({ isActive }) => isActive ? actstyle : undefined}>
+                        <div>메뉴</div>
+                    </NavLink>
+                    <NavLink to="/" style={({ isActive }) => isActive ? actstyle : undefined}>
+                        <div>메뉴</div>
+                    </NavLink>
+                </div>
+            </div>
+            <Outlet />
+        </div>
+    )
 }
 
 export default OwnerLayout
--- funniture-fe/src/layouts/UserLayout.js
+++ funniture-fe/src/layouts/UserLayout.js
@@ -1,5 +1,37 @@
+import { Outlet, useLocation } from "react-router-dom"
+import Header from "../component/Header"
+import Footer from "../component/Footer"
+import './userLayout.css'
+import banner1 from '../assets/images/banner(1).png'
+
 function UserLayout() {
 
+    const location = useLocation();
+
+    return (
+        <div className="layout">
+            <Header />
+            {location.pathname == '/' ? (
+                <div>
+                    < img className='bannerImg' src={banner1} alt="배너 사진 1" />
+                    <div className="categoryBtns">
+                        <div>카테고리</div>
+                        <div>카테고리</div>
+                        <div>카테고리</div>
+                        <div>카테고리</div>
+                        <div>카테고리</div>
+                        <div>카테고리</div>
+                        <div>카테고리</div>
+                        <div>카테고리</div>
+                    </div>
+                </div>
+            ) : null}
+            <div className="mainContent" style={location.pathname != '/' ? { "marginTop": "2.5%" } : null}>
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default UserLayout
