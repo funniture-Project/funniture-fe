ba60610e38f6ee57055ffb129922c796e5a33d99
cookiecookiekim <kimkunam06@gmail.com>
2025-02-18 18:13:00 +0900

    🌐✨[PR]2025/02/18 로그인 한 유저의 대한 정보 가져오기 - 김규남✨🌐

--- funniture-fe/src/redux/modules/MemberModule.js
+++ funniture-fe/src/redux/modules/MemberModule.js
@@ -6,10 +6,12 @@ const initialState = [];
 // 액션 정의
 export const POST_REGISTER = 'member/POST_REGISTER';
 export const POST_LOGIN = 'member/POST_LOGIN';
+export const SET_MEMBER_LIST = 'member/SET_MEMBER_LIST';
 
 const actions = createActions({
     [POST_REGISTER] : () => {},
-    [POST_LOGIN] : () => {}
+    [POST_LOGIN] : () => {},
+    [SET_MEMBER_LIST] : () => {}
 });
 
 const memberReducer = handleActions({
@@ -21,6 +23,10 @@ const memberReducer = handleActions({
     [POST_LOGIN] : (state, {payload}) => {
         console.log('memberReducer POST_LOGIN state : ', state);
         console.log('memberReducer POST_LOGIN {payload} : ', payload);
+    },
+    [SET_MEMBER_LIST] : (state, {payload}) => {
+        console.log('memberReducer SET_MEMBER_LIST state : ', state);
+        console.log('memberReducer SET_MEMBER_LIST {payload} : ', payload);
     }
 }, initialState);
 
