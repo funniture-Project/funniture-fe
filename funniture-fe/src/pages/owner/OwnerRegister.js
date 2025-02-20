784aa2d41373bc244d71ccfdab1cfb2843e08280
jin <jin200126@gmail.com>
2025-02-20 18:41:40 +0900

    :lipstick: 제공자 상품 등록 페이지 css수정 및 연결 추가 :lipstick:

--- funniture-fe/src/pages/owner/OwnerRegister.js
+++ funniture-fe/src/pages/owner/OwnerRegister.js
@@ -1,6 +1,55 @@
+import { useEffect, useState } from 'react';
 import OwRegisterCss from './ownerRegister.module.css'
+import { getSubAllCategory } from '../../apis/ProductAPI';
 
 function OwnerRegister() {
+
+    const [productName, setProductName] = useState('');
+    const [refCategory, setRefCategory] = useState(1)
+
+    const [allCategoryList, setAllCategoryList] = useState([])
+    const [categoryList, setCategoryList] = useState([])
+
+    useEffect(() => {
+        async function getData() {
+            const response = await getSubAllCategory()
+
+            setAllCategoryList(response?.results.result)
+
+            const ho = response?.results.result.filter(category => category.refCategoryCode == refCategory)
+            setCategoryList(ho)
+        }
+
+        getData()
+    }, [])
+
+    useEffect(() => {
+        settingCategoryList();
+    }, [refCategory])
+
+    function settingCategoryList() {
+        const filtered = allCategoryList.filter(category => category.refCategoryCode == refCategory)
+        setCategoryList(filtered)
+    }
+
+    // 등록 이미지 미리보기
+    function showImg(e) {
+        let file = e.target.files[0]
+        if (!file) return;
+
+        const imgShowBox = document.getElementById("imgShowBox")
+
+        let reader = new FileReader();
+
+        reader.onload = (e) => {
+            console.log("e.target : ", e.target)
+            imgShowBox.style.backgroundImage = `url(${e.target.result})`
+        }
+
+        if (file) {
+            reader.readAsDataURL(file)
+        }
+    }
 
     return (
         <div className={OwRegisterCss.wholeContainer}>
@@ -10,10 +59,10 @@ function OwnerRegister() {
                 <div className={OwRegisterCss.InfoInputTitle}>노출 상품명</div>
                 <div className={OwRegisterCss.description}>사용자에게 노출되는 상품명 입니다.</div>
                 <div className={OwRegisterCss.productNameBox}>
-                    <input type="text" />
+                    <input type="text" maxLength={20} onChange={(e) => { setProductName(e.target.value) }} />
                     <div>
-                        <div>0</div>
-                        <div> / 100</div>
+                        <div>{productName.length}</div>
+                        <div>/ 20</div>
                     </div>
                 </div>
             </div>
@@ -23,15 +72,17 @@ function OwnerRegister() {
                 <div className={OwRegisterCss.InfoInputTitle}>카테고리</div>
                 <div className={OwRegisterCss.description}>필터링에 사용되는 카테고리 입니다.</div>
                 <div className={OwRegisterCss.productCategoryBox}>
-                    <select name="refCategory" id="refCategory">
-                        <option value="">상위 카테고리</option>
+                    <select
+                        name="refCategory"
+                        id="refCategory"
+                        value={refCategory}
+                        onChange={(e) => setRefCategory(Number(e.target.value))}
+                    >
                         <option value="1">가전</option>
                         <option value="2">가구</option>
                     </select>
                     <select name="category" id="category">
-                        <option value="">상세 카테고리</option>
-                        <option value="3">카테고리 1</option>
-                        <option value="4">카테고리 1</option>
+                        {categoryList.map(category => <option value={category.categoryCode}>{category.categoryName}</option>)}
                     </select>
                 </div>
             </div>
@@ -41,8 +92,9 @@ function OwnerRegister() {
                 <div className={OwRegisterCss.InfoInputTitle}>대표이미지</div>
                 <div className={OwRegisterCss.description}>사용자에게 가장먼저 노출되는 상품 사진입니다.</div>
                 <div className={OwRegisterCss.productImgBox}>
-                    <div></div>
-                    <input type="file" />
+                    <div id='imgShowBox'></div>
+                    <input type="file" id='uploadImg' name='uploadImg' onChange={(e) => showImg(e)} />
+                    <label htmlFor="uploadImg">파일선택</label>
                 </div>
             </div>
 
@@ -51,7 +103,7 @@ function OwnerRegister() {
                 <div className={OwRegisterCss.InfoInputTitle}>정상 구매가</div>
                 <div className={OwRegisterCss.description}>일반적으로 구매했을 때의 가격을 작성해주세요</div>
                 <div className={OwRegisterCss.productPriceBox}>
-                    <input type="number" />
+                    <input type="number" min={0} defaultValue={0} />
                     <div>원</div>
                 </div>
             </div>
@@ -84,11 +136,11 @@ function OwnerRegister() {
                             </select>
                         </div>
                         <div className={OwRegisterCss.rentalCost}>
-                            <input type="number" />
+                            <input type="number" min={0} defaultValue={0} />
                             <div>원</div>
                         </div>
                         <div className={OwRegisterCss.rentalAS}>
-                            <input type="number" />
+                            <input type="number" min={0} defaultValue={0} />
                             <div>번</div>
                         </div>
                         <div className={OwRegisterCss.removeBtn}>
@@ -126,9 +178,7 @@ function OwnerRegister() {
                 <div className={OwRegisterCss.InfoInputTitle}>상세 설명</div>
                 <div className={OwRegisterCss.description}>상품에 대한 설명을 작성해주세요.</div>
                 <div className={OwRegisterCss.productDescriptionBox}>
-                    <textarea name="" id="">sada
-                        fsdfsd
-                        cccd
+                    <textarea name="" id="">
                     </textarea>
                 </div>
             </div>
@@ -139,7 +189,7 @@ function OwnerRegister() {
                 <div className={OwRegisterCss.description}>필터링에 사용되는 카테고리 입니다.</div>
                 <div className={OwRegisterCss.productCategoryBox}>
                     <div className={OwRegisterCss.stockBox}>
-                        <input type="number" />
+                        <input type="number" min={0} defaultValue={0} />
                         <div>개</div>
                     </div>
                 </div>
