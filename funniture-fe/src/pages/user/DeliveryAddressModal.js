8466d5bf71e8ae2046f77d22dac98ea9108af0fe
DESKTOP-OSFKUPN <dmssl2486@gmail.com>
2025-02-25 14:31:56 +0900

    :sparkles:[PR]2025/02/25 배송지 전체 조회 - 백&프론트 연결-정은미:sparkles:

--- funniture-fe/src/pages/user/DeliveryAddressModal.js
+++ funniture-fe/src/pages/user/DeliveryAddressModal.js
@@ -1,12 +1,16 @@
 import DeliveryAddressCss from "./deliveryAddressModal.module.css";
 import { useState } from "react";
 
-function DeliveryAddressModal() {
+function DeliveryAddressModal({deliveryAddressList, onAddressSelect}) {
 
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
     const toggleDropdown = () => {
         setIsDropdownVisible(!isDropdownVisible);
+    };
+
+    const handleAddressClick = (address) => {
+        onAddressSelect(address);  // 선택된 배송지 업데이트
     };
 
     return(
@@ -35,42 +39,56 @@ function DeliveryAddressModal() {
             {isDropdownVisible && (
                 <div className={DeliveryAddressCss.dropdownContent}>
                     <div>
-                        <div>배송지 이름 : <input type="text" placeholder="이름 입력" /></div>
+                        <div>배송지 이름 : <input type="text"/></div>
                         <div>등록</div>
                     </div>
                     <div>
-                        <div>받는 분 : <input type="text" placeholder="이름 입력" /></div>
+                        <div>받는 분 : <input type="text"/></div>
                     </div>
                     <div>
                         <div>주소 : </div>
                         <div>주소찾기</div>
                     </div>
                     <div>
-                        <div>상세주소 : <input type="text" placeholder="이름 입력" /></div>
+                        <div>상세주소 : <input type="text"/></div>
                     </div>
                 </div>
             )}
 
-            <div className={DeliveryAddressCss.addressTitle}>
-                <div>
-                    <div>
-                        <div>정은미(은미네)</div>
-                        <div>기본배송지</div>
+            {deliveryAddressList.length > 0 ? (
+                deliveryAddressList.map((address) => (
+                    <div className={DeliveryAddressCss.addressInfoContainer} key={address.destinationNo}>
+                        <div className={DeliveryAddressCss.addressTitle}>
+                            
+                            <div>
+                                <div>
+                                    <div>{address.receiver} ({address.destinationName})</div>
+                                    {address.isDefault === 1 && <div>기본배송지</div>}
+                                </div>
+                                <div>
+                                {address.isDefault === 1 
+                                    ? <div className={DeliveryAddressCss.defaultAddress}><span>✔</span> 선택됨</div>
+                                    : <div className={DeliveryAddressCss.otherAddress}>선택</div>
+                                }
+                                </div>
+                            </div>                           
+                        </div>
+
+                        <div className={DeliveryAddressCss.addressInfo}>
+                            <div>{address.destinationPhone}</div>
+                            <div>{address.destinationAddress}</div>
+                            <div>
+                                <div>수정</div>
+                                <div>삭제</div>
+                            </div>
+                        </div>
+
+                        <hr className={DeliveryAddressCss.addressHr} />
                     </div>
-                    <div>
-                        <div>선택</div>
-                    </div>
-                </div>
-            </div>
-            <div className={DeliveryAddressCss.addressInfo}>
-                <div>010-4125-3938</div>
-                <div>서울 서대문구 연세로 8-1 버티고타워 7층</div>
-                <div>
-                    <div>수정</div>
-                    <div>삭제</div>
-                </div>  
-            </div>
-            <hr className={DeliveryAddressCss.addressHr}/>
+                ))
+            ) : (
+                <div>배송지가 없습니다.</div>
+            )}
         </div>
     );
 }
