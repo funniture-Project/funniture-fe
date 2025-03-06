81dae6db014c18284c2a70d31311db6468f39227
cookiecookiekim <kimkunam06@gmail.com>
2025-03-06 14:21:45 +0900

    ✨[PR]2025/03/06 상품 문의 등록 연결  - 김규남 ✨

--- funniture-fe/src/apis/InquiryAPI.js
+++ funniture-fe/src/apis/InquiryAPI.js
@@ -1,22 +1,28 @@
 import api from "./Apis";
 
 // 상세 페이지 문의 불러오기
+// 상세 페이지 문의 불러오기
 export const callInquiryByProductNoAPI = (productNo) => {
     const requestURL = `http://localhost:8080/api/v1/inquiry/product/${productNo}`;
 
     return async (dispatch) => {
         const response = await api.get(requestURL);
         console.log('productNo로 문의 조회 서버에 잘 다녀 왔나 response : ', response);
-        return response;
+
+        // 데이터를 명시적으로 반환
+        return response.data;
     };
 };
 
-export const callInquiryInsertByProductNoAPI = (productNo) => {
-    const requestURL = `http://localhost:8080/api/v1/inquiry/product/${productNo}`;
+
+// 문의 등록
+export const callInquiryRegistByProductNoAPI = (dataToSend, memberId) => {
+    const requestURL = `http://localhost:8080/api/v1/inquiry/regist`;
 
     return async (dispatch) => {
-        const response = await api.get(requestURL);
-        console.log('productNo로 문의 조회 서버에 잘 다녀 왔나 response : ', response);
+        const response = await api.post(requestURL, { ...dataToSend, memberId });
+        console.log('문의 등록 결과 서버에 잘 다녀 왔나 response : ', response);
         return response;
     };
 };
+
